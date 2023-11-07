8ef3a47eaaf6e9a16b8ceda7a0e11d231eb4a49c
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-11-07 19:25:06 +0300

    #17___________Фильтрация фильмов по жанру и году, класс Q в Django.

--- django_movie/movies/urls.py
+++ django_movie/movies/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     # name для вставки в html {% url 'movie_detail' %}
     path("", views.MoviesView.as_view(), name='movies'),
+    # Размещён перед шаблоном поиском фильма по слагу чтобы не было ошибки с переменной queryset
+    # url - для фильтра фильмов по году или жанру
+    path("filter/", views.FilterMoviesView.as_view(), name='filter'),
     path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
     path("review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
     path("actor/<str:slug>/", views.ActorView.as_view(), name="actor_detail"),
--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,12 +1,23 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Movie, Category, Actor
+from .models import Movie, Category, Actor, Genre
 from . forms import ReviewForm
 
 
-class MoviesView(ListView):
+class GenreYears:
+    """ Жанры и года"""
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        # Функция values возвращает указанные словари, values_list кортежи
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GenreYears, ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
@@ -23,7 +34,7 @@ class MoviesView(ListView):
     #     return context
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYears, DetailView):
     """Полное описание фильма"""
     model = Movie
     # Отвечает за поле по которому будет происходить поиск записи,
@@ -71,9 +82,19 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYears, DetailView):
     """ Вывод информации о актёре. """
     model = Actor
     template_name = "movies/actor.html"
     # Поле по которому мы будем искать актёров
     slug_field = "name"
+
+
+class FilterMoviesView(GenreYears, ListView):
+    """Фильтр фильмов"""
+    def get_queryset(self):
+        # getlist - выбрать все зночения атрибута
+        queryset = Movie.objects.filter(Q(year__in=self.request.GET.getlist("year")) |
+                                        Q(genres__in=self.request.GET.getlist("genre")))
+        return queryset
+
