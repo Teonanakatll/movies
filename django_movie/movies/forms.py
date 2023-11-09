179fd422f5362a4dfb31305da8a81c0946ba5d74
Teonanakatll <105820090+Teonanakatll@users.noreply.github.com>
2023-11-09 16:07:01 +0300

    #19.___________Рейтинг фильмов.

--- django_movie/movies/forms.py
+++ django_movie/movies/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
-from . models import Reviews
+from .models import Reviews, RatingStar, Rating
+
 
 # Если необходимо указывать в форме поля не связанные с моделями то наследуемся от forms
 class ReviewForm(forms.ModelForm):
@@ -8,4 +9,17 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Reviews
         fields = ("name", "email", "text")
+
+
+class RatingForm(forms.ModelForm):
+    """Форма добавления рейтинга"""
+    # Переопределяем поле star и с помощью ModelChoiceField передаём в него все экземпляры модели
+    # RatingStar и виджет - радиокнопку (или можно выбрать выпадающий список или чекбокс)
+    star = forms.ModelChoiceField(
+        queryset=RatingStar.objects.all(), widget=forms.RadioSelect(), empty_label=None
+    )
+
+    class Meta:
+        model = Rating
+        fields = ("star",)
 
